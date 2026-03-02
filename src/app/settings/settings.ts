98c1280a139e422c9b20cb32e7b212602a4c3e94
Faizal Hassan <hassanfaizal047@gmail.com>
2026-03-02 20:48:58 +0500

    fixed bugs

--- src/app/settings/settings.ts
+++ src/app/settings/settings.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
